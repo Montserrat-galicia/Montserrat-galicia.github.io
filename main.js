ade9487384b885f2349731030ffe763a42a21dc2
Montserrat Galicia <185145@upslp.edu.mx>
2024-07-31 21:16:09 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ var typewriter = new Typewriter(app, {
 typewriter.typeString('<h2>Desarrolladora Fronted Jr. | Estudiante de Ingeniería en Telemática</h2>')
     .pauseFor(2500)
     .deleteAll()
-    .typeString('¡Si lo puedes crear, lo puedes imaginar!')
+    .typeString('¡Si lo puedes imaginar, lo puedes crear!')
     .pauseFor(2500)
-    .deleteChars(9)
+    .deleteChars(6)
     .typeString('<strong>programar!</strong>')
     .pauseFor(2500)
     .start();
